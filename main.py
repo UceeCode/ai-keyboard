b484b06cbe135e6dba166a30f18dce8792360b25
Franklin <writefranklyn@gmail.com>
2025-04-26 17:06:49 +0100

    add clicking logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import pyautogui
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
@@ -44,18 +45,61 @@ for i in range(len(keys)):
     for j, key in enumerate(keys[i]):
         buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
 
+clicked = False
 
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    lmList = []
+
     hands, img = detector.findHands(img)
     if hands:
-        hand = hands[0]  # Pick the first detected hand
+        hand = hands[0]
         lmList = hand["lmList"]  # List of 21 landmark points
         bbox = hand["bbox"]  # Bounding box info
         center = hand["center"]  # Center of hand
         handType = hand["type"]  # Left or Right hand
-
     img = drawAll(img, buttonList)
+
+    if lmList:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+
+
+            if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
+                # Highlight button green when finger is on it
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 240, 0), thickness=-1)
+
+                # Redraw the text after highlighting
+                font = cv2.FONT_HERSHEY_PLAIN
+                font_scale = 3
+                font_thickness = 3
+                text_size = cv2.getTextSize(button.text, font, font_scale, font_thickness)[0]
+                text_x = x + (w - text_size[0]) // 2
+                text_y = y + (h + text_size[1]) // 2
+                cv2.putText(img, button.text, (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness)
+
+                l, _, _ = detector.findDistance(lmList[8][:2], lmList[12][:2], img)
+
+                # If fingers are close together, register a click
+                if l < 40:
+                    clicked = True
+                    pyautogui.write(button.text)
+                    print(button.text)
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=-1)
+                    font = cv2.FONT_HERSHEY_PLAIN
+                    font_scale = 3
+                    font_thickness = 3
+                    text_size = cv2.getTextSize(button.text, font, font_scale, font_thickness)[0]
+                    text_x = x + (w - text_size[0]) // 2
+                    text_y = y + (h + text_size[1]) // 2
+                    cv2.putText(img, button.text, (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness)
+
+                if l > 40:
+                    clicked = False
+
 
     cv2.imshow("image", img)
     cv2.waitKey(1)
